6e78ec541d37fb13db8968200175b180ce33ec0f
Dushyant Singh <32354434+DushyantC137@users.noreply.github.com>
2020-01-01 02:34:14 +0530

    Implemented Flocking
    implemented Flocking behavior for cohesion , separation and Allignment
    And A script to run multiple flockAgents

---
+++ Musical Flocking/Assets/Scripts/FlockBehaviour.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FlockBehaviour : MonoBehaviour
+{
+    public Vector3 baseRotation;
+
+    [Range(0, 50)]
+    public float maxSpeed = 1f;
+
+    [Range(.1f, .5f)]
+    public float maxForce = .03f;
+
+    [Range(1, 10)]
+    public float neighborhoodRadius = 3f;
+
+    [Range(0, 3)]
+    public float separationAmount = 1f;
+
+    [Range(0, 3)]
+    public float cohesionAmount = 1f;
+
+    [Range(0, 3)]
+    public float alignmentAmount = 1f;
+
+    public Vector2 acceleration;
+    public Vector2 velocity;
+
+    private Vector2 Position
+    {
+        get
+        {
+            return gameObject.transform.position;
+        }
+        set
+        {
+            gameObject.transform.position = value;
+        }
+    }
+
+    private void Start()
+    {
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle) + baseRotation);
+        velocity = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+    }
+
+    private void Update()
+    {
+        var boidColliders = Physics2D.OverlapCircleAll(Position, neighborhoodRadius);
+        var boids = boidColliders.Select(o => o.GetComponent<FlockBehaviour>()).ToList();
+        boids.Remove(this);
+
+        Flock(boids);
+        UpdateVelocity();
+        UpdatePosition();
+        UpdateRotation();
+        WrapAround();
+    }
+
+    private void Flock(IEnumerable<FlockBehaviour> boids)
+    {
+        var alignment = Alignment(boids);
+        var separation = Separation(boids);
+        var cohesion = Cohesion(boids);
+
+        acceleration = alignmentAmount * alignment + cohesionAmount * cohesion + separationAmount * separation;
+    }
+
+    public void UpdateVelocity()
+    {
+        velocity += acceleration;
+        velocity = LimitMagnitude(velocity, maxSpeed);
+    }
+
+    private void UpdatePosition()
+    {
+        Position += velocity * Time.deltaTime;
+    }
+
+    private void UpdateRotation()
+    {
+        var angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle) + baseRotation);
+    }
+
+    private Vector2 Alignment(IEnumerable<FlockBehaviour> boids)
+    {
+        var velocity = Vector2.zero;
+        if (!boids.Any()) return velocity;
+
+        foreach (var boid in boids)
+        {
+            velocity += boid.velocity;
+        }
+        velocity /= boids.Count();
+
+        var steer = Steer(velocity.normalized * maxSpeed);
+        return steer;
+    }
+
+    private Vector2 Cohesion(IEnumerable<FlockBehaviour> boids)
+    {
+        if (!boids.Any()) return Vector2.zero;
+
+        var sumPositions = Vector2.zero;
+        foreach (var boid in boids)
+        {
+            sumPositions += boid.Position;
+        }
+        var average = sumPositions / boids.Count();
+        var direction = average - Position;
+
+        var steer = Steer(direction.normalized * maxSpeed);
+        return steer;
+    }
+
+    private Vector2 Separation(IEnumerable<FlockBehaviour> boids)
+    {
+        var direction = Vector2.zero;
+        boids = boids.Where(o => DistanceTo(o) <= neighborhoodRadius / 2);
+        if (!boids.Any()) return direction;
+
+        foreach (var boid in boids)
+        {
+            var difference = Position - boid.Position;
+            direction += difference.normalized / difference.magnitude;
+        }
+        direction /= boids.Count();
+
+        var steer = Steer(direction.normalized * maxSpeed);
+        return steer;
+    }
+
+    private Vector2 Steer(Vector2 desired)
+    {
+        var steer = desired - velocity;
+        steer = LimitMagnitude(steer, maxForce);
+
+        return steer;
+    }
+
+    private float DistanceTo(FlockBehaviour boid)
+    {
+        return Vector3.Distance(boid.transform.position, Position);
+    }
+
+    private Vector2 LimitMagnitude(Vector2 baseVector, float maxMagnitude)
+    {
+        if (baseVector.sqrMagnitude > maxMagnitude * maxMagnitude)
+        {
+            baseVector = baseVector.normalized * maxMagnitude;
+        }
+        return baseVector;
+    }
+
+    private void WrapAround()
+    {
+        if (Position.x < -9) Position = new Vector2(9, Position.y);
+        if (Position.y < -5.43) Position = new Vector2(Position.x, 5.43f);
+        if (Position.x > 9) Position = new Vector2(-9, Position.y);
+        if (Position.y > 5.43f) Position = new Vector2(Position.x, -5.43f);
+    }
+}
---
+++ Musical Flocking/Assets/Scripts/InstantiateFlock.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class InstantiateFlock : MonoBehaviour
+{
+    public GameObject fishPrefab;
+
+    [Range(0, 300)]
+    public int number;
+
+    private void Start()
+    {
+        for (int i = 0; i < number; i++)
+        {
+            Instantiate(fishPrefab, Vector3.zero, Quaternion.identity);
+        }
+    }
+}
